11f2314dc9be2a351200b39c95cbfa109e7810cd
tadarim <2560285440@qq.com>
2024-05-21 13:50:46 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <RobotModel :action="action"></RobotModel>
+    <button @click="stateChangeHandler">WalkJump</button>
+    <button @click="actionChangeHandler">Jump</button>
+  </div>
+</template>
+
+<script>
+import RobotModel from './components/Robot'
+
+export default {
+  components: {
+    RobotModel
+  },
+  data() {
+    return {
+      // ['Idle', 'Walking', 'Running', 'Dance', 'Death', 'Sitting', 'Standing', 'Jump', 'Yes', 'No', 'Wave', 'Punch', 'ThumbsUp']
+      action: 'Idle'
+    }
+  },
+  methods: {
+    stateChangeHandler() {
+      this.action = 'WalkJump'
+    },
+    actionChangeHandler() {
+      this.action = 'Jump'
+    },
+  }
+
+}
+</script>
+
+<style scoped></style>
